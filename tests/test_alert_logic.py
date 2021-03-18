bd57cb7030b41789a20412a503559d04d374ff09
Jan Harenburg <jan@harenburg.com>
2021-03-18 20:58:20 +0100

    test: fixed/added test case

--- tests/test_alert_logic.py
+++ tests/test_alert_logic.py
@@ -108,13 +108,13 @@ def test_price_increase_below_init_price(tmp_path):
     p = d / file_name
 
     market = 'ETH-EUR'
-    ticker_price = Decimal('800')
+    ticker_price = Decimal('950')
 
     alert = copy.deepcopy(alert_template)
     alert['actions'] = []
     alert['market'] = market
     alert['init_price'] = Decimal('1000')
-    alert['trailing_percentage'] = Decimal('0.9')
+    alert['trailing_percentage'] = Decimal('0.7')
     alert['status'] = 'active'
     alert['price'] = Decimal('900')
     alert['trailing_price'] = alert['trailing_percentage'] * alert['init_price']
@@ -139,9 +139,51 @@ def test_price_increase_below_init_price(tmp_path):
     assert ah.alerts[0]['init_price'] == alert['init_price']
     assert ah.alerts[0]['market'] == market
     assert ah.alerts[0]['price'] == ticker_price
-    assert ah.alerts[0]['status'] == 'hit'
+    assert ah.alerts[0]['status'] == 'active'
     assert ah.alerts[0]['trailing_percentage'] == alert['trailing_percentage']
     assert ah.alerts[0]['trailing_price'] == alert['trailing_price']
+
+
+def test_price_increase_above_init_price(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / file_name
+
+    market = 'ETH-EUR'
+    ticker_price = Decimal('1200')
+
+    alert = copy.deepcopy(alert_template)
+    alert['actions'] = []
+    alert['market'] = market
+    alert['init_price'] = Decimal('1000')
+    alert['trailing_percentage'] = Decimal('0.9')
+    alert['status'] = 'active'
+    alert['price'] = Decimal('1100')
+    alert['trailing_price'] = alert['trailing_percentage'] * alert['price']
+
+    file_content = json.dumps([alert], indent=4, sort_keys=True)
+
+    p.write_text(file_content)
+
+    ticker_prices = [
+        {
+            'market': market,
+            'price': ticker_price
+        }
+    ]
+
+    ah = AlertHandler(ticker_prices=ticker_prices, alerts_file_name=p)
+    ah.process_alerts()
+    ah.save_alerts()
+    ah.load_alerts()
+
+    assert ah.alerts[0]['actions'] == []
+    assert ah.alerts[0]['init_price'] == alert['init_price']
+    assert ah.alerts[0]['market'] == market
+    assert ah.alerts[0]['price'] == ticker_price
+    assert ah.alerts[0]['status'] == 'active'
+    assert ah.alerts[0]['trailing_percentage'] == alert['trailing_percentage']
+    assert ah.alerts[0]['trailing_price'] == alert['trailing_percentage'] * ticker_price
 
 
 def test_price_decreased():
