f8f461cbf3a9de6a0806824f7c142c1abfb8a196
mandano <mandanothedano@posteo.de>
2021-03-27 15:32:58 +0100

    fix: use correct dotenv path

--- handle_alerts.py
+++ handle_alerts.py
@@ -14,8 +14,8 @@ from python_bitvavo_api.bitvavo import Bitvavo
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '/.env')
-load_dotenv('.env')
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 logging.basicConfig(level=os.environ.get("LOGGING_LEVEL"))
 
