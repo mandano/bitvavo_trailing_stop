109e75f85a10e090ae2cdfc245bceae6656d116d
mandano <mandanothedano@posteo.de>
2021-08-05 10:05:35 +0200

    feat: added fixture with bitvavo test credentials

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture
+def bitvavo_credentials():
+    return {
+        'bitvavo_access_key': '34webnghjyu645etrfghfjyu5terwfsdsdffdsfdasq2345q34wetsdfxfgawetf',
+        'bitvavo-access-signature': '7ac3ece85a99055fd2d46d9979f337c399e290265f0411473155771043faa138'
+    }
