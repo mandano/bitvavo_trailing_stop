491e9296d377265ef6837a6c1dd3898e4af334aa
mandano <mandanothedano@posteo.de>
2021-08-05 10:07:28 +0200

    fix: define api keys so bitvavo construction does not fail

--- tests/unit/models/clients/test_bitvavo.py
+++ tests/unit/models/clients/test_bitvavo.py
@@ -3,21 +3,30 @@ from decimal import Decimal
 from models.clients.Bitvavo import BitvavoClient
 
 
-def test_get_ticker_price_no_market():
+def test_get_ticker_price_no_market(bitvavo_credentials):
+    market = "BTC-EUR"
+
     b_client = BitvavoClient(
+        api_key=bitvavo_credentials['bitvavo_access_key'],
+        api_secret=bitvavo_credentials['bitvavo-access-signature'],
         _response_ticker_price={
-            "market": "BTC-EUR",
+            "market": market,
             "price": "5003.2"
         }
     )
 
-    assert b_client.get_ticker_price() == None
+    assert b_client.get_ticker_price() is None
 
 
-def test_get_ticker_price():
+def test_get_ticker_price(bitvavo_credentials):
+    market = "BTC-EUR"
+
     b_client = BitvavoClient(
+        api_key=bitvavo_credentials['bitvavo_access_key'],
+        api_secret=bitvavo_credentials['bitvavo-access-signature'],
+        market=market,
         _response_ticker_price={
-            "market": "BTC-EUR",
+            "market": market,
             "price": "5003.2"
         }
     )
